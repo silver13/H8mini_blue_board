3661f43c933f0ca131bfa148ff2ed46558f0a7b0
silverag123 <arminio_grgic@yahoo.com>
2017-05-20 01:29:03 +0200

    Added support for combined roll+pitch tuning

--- Silverware/src/rx_bayang_ble_app.c
+++ Silverware/src/rx_bayang_ble_app.c
@@ -790,7 +790,11 @@ buf[L++] = packetpersecond_short;
 /*
 buf[L++] =  onground_and_bind; //binary xxxxabcd - xxxx = error code or warning, a -> 0 = stock TX, 1= other TX, b -> 0 = not failsafe, 1 = failsafe, c = 0 -> not bound, 1 -> bound, d = 0 -> in the air, 1 = on the ground;
 */
-buf[L++] =  (rate_and_mode_value<<4)+onground_and_bind; //binary xxRMabcd - xx = error code or warning, R = rate (0 - normal, 1 - fast) , M = mode (1 - level, 0 - acro); a -> 0 = stock TX, 1= other TX, b -> 0 = not failsafe, 1 = failsafe, c = 0 -> not bound, 1 -> bound, d = 0 -> in the air, 1 = on the ground;
+#ifdef COMBINE_PITCH_ROLL_PID_TUNING
+	buf[L++] =  B01000000+((rate_and_mode_value<<4)+onground_and_bind); //binary xxRMabcd - x = error code or warning, 1 = combined roll+pitch tuning, R = rate (0 - normal, 1 - fast) , M = mode (1 - level, 0 - acro); a -> 0 = stock TX, 1= other TX, b -> 0 = not failsafe, 1 = failsafe, c = 0 -> not bound, 1 -> bound, d = 0 -> in the air, 1 = on the ground;
+#else
+	buf[L++] =  (rate_and_mode_value<<4)+onground_and_bind; //binary x0RMabcd - x = error code or warning, 0 = no combined roll+pitch tuning, R = rate (0 - normal, 1 - fast) , M = mode (1 - level, 0 - acro); a -> 0 = stock TX, 1= other TX, b -> 0 = not failsafe, 1 = failsafe, c = 0 -> not bound, 1 -> bound, d = 0 -> in the air, 1 = on the ground;
+#endif
 	
 buf[L++] =  vbatt_comp_int>>8;  // Battery voltage compensated
 buf[L++] =  vbatt_comp_int;  // Battery voltage compensated
